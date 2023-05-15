3077f8144a2b21a20587cd9c01579f823a08b355
Jérémy Laurent <contact@jeremy-laurent.com>
2023-05-15 15:58:06 +0200

    build(*): Initial PWA commit

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'PleaseUsePWA',
+                short_name: 'PleaseUsePWA',
+                description: 'Progressive Web Apps are the best.',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
+})
