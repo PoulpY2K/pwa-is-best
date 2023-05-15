b75000ef601f2ad9e0081f3e299bb094233aaae5
Jérémy Laurent <contact@jeremy-laurent.com>
2023-05-15 16:41:03 +0200

    feat(validation): add install validation

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,9 @@ onBeforeUnmount(() => {
 <template>
     <div class="flex flex-col justify-center items-center min-h-screen bg-hero-pattern">
         <div class="bg-opacity-60"></div>
-        <div class="text-center text-neutral-content">
+
+        <!--    Le bouton pourrait déclencher une animation pour montrer à l'utilisateur comment installer la PWA une fois le bouton cliqué    -->
+        <div class="text-center text-neutral-content mb-20">
             <div class="max-w-2xl">
                 <h1 class="mb-2 text-5xl font-semibold">Bienvenue sur une</h1>
                 <h1 class="mb-5 text-6xl font-bold text-purple-500">Progressive Web App</h1>
@@ -47,6 +49,36 @@ onBeforeUnmount(() => {
                     Installez moi !
                 </button>
             </div>
+        </div>
+
+        <!--    La jauge devrait évoluer si l'utilisateur lance l'application installée et non pas la version web,
+        avec un petit son joué afin de lui faire comprendre que tout fonctionne bien -->
+        <div>
+            <ol class="items-center sm:flex">
+                <li class="relative mb-6 sm:mb-0">
+                    <div class="flex items-center">
+                        <div class="z-10 flex items-center justify-center w-6 h-6 bg-red-500 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0"></div>
+                        <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                    </div>
+                    <div class="mt-3 sm:pr-8">
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Vous n'avez pas installé
+                            l'application...</h3>
+                        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Prenez le temps d'installer
+                            l'application pour pouvoir l'utiliser pleinement.</p>
+                    </div>
+                </li>
+                <li class="relative mb-6 sm:mb-0">
+                    <div class="flex items-center">
+                        <div class="z-10 flex items-center justify-center w-6 h-6 bg-green-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0"></div>
+                    </div>
+                    <div class="mt-3 sm:pr-8">
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Vous avez bien installé
+                            l'application !</h3>
+                        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Bravo ! Vous pouvez désormais
+                            découvrir les joies de la PWA !</p>
+                    </div>
+                </li>
+            </ol>
         </div>
     </div>
 </template>
